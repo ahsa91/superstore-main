7c820424d80dfdfe978d8f740aee137d0ba591ab
ahsa91 <ahsa91@gmail.com>
2022-04-06 00:51:16 +0100

    dashboard fragment assets update adjustment of navigation components renaming notifications and home to oders and products respectively

--- app/src/main/java/com/superstore/ui/fragments/OrdersFragment.kt
+++ app/src/main/java/com/superstore/ui/fragments/OrdersFragment.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.superstore.R
 
-class NotificationsFragment : Fragment() {
+class OrdersFragment : Fragment() {
 
 
     /*private lateinit var notificationsViewModel: NotificationsViewModel*/
@@ -21,9 +21,9 @@ class NotificationsFragment : Fragment() {
         /*notificationsViewModel =
             ViewModelProviders.of(this).get(NotificationsViewModel::class.java)*/
 
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val root = inflater.inflate(R.layout.fragment_orders, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
-        textView.text = "This is notifications Fragment"
+        textView.text = "This is orders Fragment"
 
         /*notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
--- app/src/main/java/com/superstore/ui/fragments/ProductsFragment.kt
+++ app/src/main/java/com/superstore/ui/fragments/ProductsFragment.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.superstore.R
 
-class HomeFragment : Fragment() {
+class ProductsFragment : Fragment() {
 
 
     /*private lateinit var homeViewModel: HomeViewModel*/
@@ -21,9 +21,9 @@ class HomeFragment : Fragment() {
         /*homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)*/
 
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val root = inflater.inflate(R.layout.fragment_products, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        textView.text = "This is home Fragment"
+        textView.text = "This is Products Fragment"
 
         /*homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
