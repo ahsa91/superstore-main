252652cc478f5b641fd46e938ffb30d55f6a2ec7
ahsa91 <ahsa91@gmail.com>
2022-04-07 07:52:00 +0100

    user can select product image from local storage

--- app/src/main/java/com/superstore/ui/activities/AddProductActivity.kt
+++ app/src/main/java/com/superstore/ui/activities/AddProductActivity.kt
@@ -1,17 +1,44 @@
 package com.superstore.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+//import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.superstore.R
+import com.superstore.utils.Constants
+import com.superstore.utils.GlideLoader
 import kotlinx.android.synthetic.main.activity_add_product.*
+import java.io.IOException
 
-class AddProductActivity : AppCompatActivity() {
+@Suppress("DEPRECATION")
+
+class AddProductActivity : BaseActivity(), View.OnClickListener {
+    // A global variable for URI of a selected image from phone storage.
+    private var mSelectedImageFileUri: Uri? = null
+
+    // A global variable for uploaded product image URL.
+    private var mProductImageURL: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)
 
         //call setup action bar
         setupActionBar()
+
+        // Assign the click event to iv_add_update_product image.
+        iv_add_update_product.setOnClickListener(this)
+
+        // Assign the click event to submit button.
+        btn_submit.setOnClickListener(this)
     }
 
     //setup action bar
@@ -27,4 +54,142 @@ class AddProductActivity : AppCompatActivity() {
 
         toolbar_add_product_activity.setNavigationOnClickListener { onBackPressed() }
     }
+
+    //function for onClick listner
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+
+                // The permission code is similar to the user profile image selection.
+                R.id.iv_add_update_product -> {
+                    if (ContextCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                        == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        Constants.showImageChooser(this@AddProductActivity)
+                    } else {
+                        /*Requests permissions to be granted to this application. These permissions
+                         must be requested in your manifest, they should not be granted to your app,
+                         and they should have protection level*/
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+
+                R.id.btn_submit -> {
+                    if (validateProductDetails()) {
+
+                        uploadProductImage()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun validateProductDetails(): Boolean {
+        return when {
+
+            mSelectedImageFileUri == null -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_select_product_image), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_product_title.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_product_title), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_product_price.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_product_price), true)
+                false
+            }
+
+            TextUtils.isEmpty(et_product_description.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_enter_product_description),
+                    true
+                )
+                false
+            }
+
+            TextUtils.isEmpty(et_product_quantity.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_enter_product_quantity),
+                    true
+                )
+                false
+            }
+            else -> {
+                true
+            }
+        }
+    }
+
+    private fun uploadProductImage(){}
+
+
+
+    /**
+     * This function will identify the result of runtime permission after the user allows or deny permission based on the unique code.
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            //If permission is granted
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Constants.showImageChooser(this@AddProductActivity)
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(
+                    this,
+                    resources.getString(R.string.read_storage_permission_denied),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK
+            && requestCode == Constants.PICK_IMAGE_REQUEST_CODE
+            && data!!.data != null
+        ) {
+
+            // Replace the add icon with edit icon once the image is selected.
+            iv_add_update_product.setImageDrawable(
+                ContextCompat.getDrawable(
+                    this@AddProductActivity,
+                    R.drawable.ic_vector_edit
+                )
+            )
+
+            // The uri of selection image from phone storage.
+            mSelectedImageFileUri = data.data!!
+
+            try {
+                // Load the product image in the ImageView.
+                GlideLoader(this@AddProductActivity).loadProductPicture(
+                    mSelectedImageFileUri!!,
+                    iv_product_image
+                )
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/superstore/utils/GlideLoader.kt
+++ app/src/main/java/com/superstore/utils/GlideLoader.kt
@@ -28,4 +28,18 @@ class GlideLoader(val context: Context) {
             e.printStackTrace()
         }
     }
+
+    //function to load image from uri or url for the product image
+    fun loadProductPicture(image: Any, imageView: ImageView) {
+        try {
+            // Load the user image in the ImageView.
+            Glide
+                .with(context)
+                .load(image) // Uri or URL of the image
+                .centerCrop() // Scale type of the image.
+                .into(imageView) // the view in which the image will be loaded.
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
 }
