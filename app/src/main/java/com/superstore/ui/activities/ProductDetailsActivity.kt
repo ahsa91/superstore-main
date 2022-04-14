9471cddf54ebb9f7ee548727b90ae71152bccde8
ahsa91 <ahsa91@gmail.com>
2022-04-14 15:15:49 +0100

    product details can be retrieved and displayed

--- app/src/main/java/com/superstore/ui/activities/ProductDetailsActivity.kt
+++ app/src/main/java/com/superstore/ui/activities/ProductDetailsActivity.kt
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.superstore.R
+import com.superstore.firestore.FirestoreClass
+import com.superstore.models.Product
 import com.superstore.utils.Constants
+import com.superstore.utils.GlideLoader
 import kotlinx.android.synthetic.main.activity_product_details.*
 
-class ProductDetailsActivity : AppCompatActivity() {
+class ProductDetailsActivity : BaseActivity() {
 
     //global variable for product ID
     private var mProductId: String = ""
@@ -25,6 +28,10 @@ class ProductDetailsActivity : AppCompatActivity() {
 
         //call  setupActionBar()
         setupActionBar()
+        //retrieve product details when activity is launched
+        getProductDetails()
+
+
     }
 
 
@@ -41,4 +48,37 @@ class ProductDetailsActivity : AppCompatActivity() {
 
         toolbar_product_details_activity.setNavigationOnClickListener { onBackPressed() }
     }
+
+    /**
+     * A function to notify the success result of the product details based on the product id.
+     *
+     * @param product A model class with product details.
+     */
+    fun productDetailsSuccess(product: Product) {
+
+        // Hide Progress dialog.
+        hideProgressDialog()
+
+        // Populate the product details in the UI.
+        GlideLoader(this@ProductDetailsActivity).loadProductPicture(
+            product.image,
+            iv_product_detail_image
+        )
+
+        tv_product_details_title.text = product.title
+        tv_product_details_price.text = "$${product.price}"
+        tv_product_details_description.text = product.description
+        tv_product_details_stock_quantity.text = product.stock_quantity
+    }
+
+    //function to call the firestore class function that will get the product details from cloud firestore based on the product id
+    private fun getProductDetails() {
+
+        // Show the product dialog
+        showProgressDialog(resources.getString(R.string.please_wait))
+
+        // Call the function of FirestoreClass to get the product details.
+        FirestoreClass().getProductDetails(this@ProductDetailsActivity, mProductId)
+    }
+
 }
