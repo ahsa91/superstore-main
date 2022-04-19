7576aef647922491ad3d5d0cbed1ebeb68fb7c74
ahsa91 <ahsa91@gmail.com>
2022-04-19 02:50:41 +0100

    sold products successfully display

--- app/src/main/java/com/superstore/ui/fragments/SoldProductsFragment.kt
+++ app/src/main/java/com/superstore/ui/fragments/SoldProductsFragment.kt
@@ -1,22 +1,20 @@
 package com.superstore.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.superstore.R
+import com.superstore.firestore.FirestoreClass
+import com.superstore.models.SoldProduct
+import com.superstore.ui.adapters.SoldProductsListAdapter
+import kotlinx.android.synthetic.main.fragment_sold_products.*
 
-
-
-
+/**
+ * Sold Products Listing Fragment.
+ */
 class SoldProductsFragment : BaseFragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +22,51 @@ class SoldProductsFragment : BaseFragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sold_products, container, false)
+    }
+
+    //Override the onResume function and call the function to get the list of sold products.
+    override fun onResume() {
+        super.onResume()
+
+        getSoldProductsList()
+    }
+
+    // function to get the list of sold products.
+
+    private fun getSoldProductsList() {
+        // Show the progress dialog.
+        showProgressDialog(resources.getString(R.string.please_wait))
+
+        // Call the function of Firestore class.
+        FirestoreClass().getSoldProductsList(this@SoldProductsFragment)
+    }
+
+
+    //  function to get the success result list of sold products.
+    /**
+     * A function to get the list of sold products.
+     */
+    fun successSoldProductsList(soldProductsList: ArrayList<SoldProduct>) {
+
+        // Hide Progress dialog.
+        hideProgressDialog()
+
+        // Populate the list in the RecyclerView using the adapter class.
+        if (soldProductsList.size > 0) {
+            rv_sold_product_items.visibility = View.VISIBLE
+            tv_no_sold_products_found.visibility = View.GONE
+
+            rv_sold_product_items.layoutManager = LinearLayoutManager(activity)
+            rv_sold_product_items.setHasFixedSize(true)
+
+            val soldProductsListAdapter =
+                SoldProductsListAdapter(requireActivity(), soldProductsList)
+            rv_sold_product_items.adapter = soldProductsListAdapter
+        } else {
+            rv_sold_product_items.visibility = View.GONE
+            tv_no_sold_products_found.visibility = View.VISIBLE
+        }
+
     }
 
 
